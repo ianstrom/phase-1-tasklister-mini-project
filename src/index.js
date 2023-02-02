dc196ed4c85977b14e1245f93163d9f99f43c2c2
ianstrom <iankstrom@gmail.com>
2023-02-01 20:05:55 -0500

    adding js for tasklist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskList = document.getElementById('tasks')
+  
+  const newTaskForm = document.getElementById('create-task-form')
+  const newTaskDescription = document.getElementById("new-task-description");
+
+  newTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newTask = document.createElement('div');
+    newTask.textContent = newTaskDescription.value
+    taskList.appendChild(newTask)
+  })
 });
